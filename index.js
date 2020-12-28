a89cc5ea01009099134c52b29370f72c1e37c1b0
John Akhilomen <jonbasanti@gmail.com>
2020-12-28 13:36:37 -0600

    Added place API

--- index.js
+++ index.js
@@ -6,21 +6,50 @@ let map;
 let service;
 let infowindow;
 
+function placeDetails(query)
+{
+  fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${query}&inputtype=textquery&fields=place_id,formatted_address,name,rating,opening_hours,geometry&key=${apiKey}`)
+  .then(response=>response.json())
+  .then(response => {
+    console.log(response);
+    const isOpened = response.candidates[0].opening_hours.open_now == false ? "No" : "Yes";
+    const contentString =
+  "<div>" +
+  "<p><b>Place id</b>: "+response.candidates[0].place_id+
+  "<p><b>Place name</b>: "+response.candidates[0].name+
+  "<br><b>Address</b>: "+response.candidates[0].formatted_address+
+  "<br><b>Rating</b>: "+response.candidates[0].rating+
+  "<br><b>Opened</b>: "+isOpened+
+  "</p></div>";
+
+    setUpMap(response.candidates[0].geometry.location.lat, 
+      response.candidates[0].geometry.location.lng,
+      contentString
+      )
+  })
+  .catch(error=>console.log(error));
+}
+
 function generateGeolocation(query){
   fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${apiKey}`)
   .then(response => response.json())
   .then(response => {
+    const contentString =
+    "<div>" +
+    "<p><b>Place id</b>: "+response.results[0].place_id+
+    "<br><b>Address</b>: "+response.results[0].formatted_address+
+    "</p></div>";
     setUpMap(response.results[0].geometry.location.lat, 
       response.results[0].geometry.location.lng,
-      response.results[0].place_id,
-      response.results[0].formatted_address)
+      contentString
+      )
   })
   .catch(err=>console.log(err));
 
 }
 
 
-function setUpMap(lat, lng, placeId, formattedAdd)
+function setUpMap(lat, lng, contentString)
 {
   const latLng = new google.maps.LatLng(lat, lng);
 
@@ -38,11 +67,11 @@ function setUpMap(lat, lng, placeId, formattedAdd)
 
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
   
-  const contentString =
+  /*const contentString =
   "<div>" +
-  "<p><b>Place id</b>: "+placeId+
+  "<p><b>Place id or name</b>: "+placeId+
   "<br><b>Address</b>: "+formattedAdd+
-  "</p></div>";
+  "</p></div>";*/
   
   const infowindow = new google.maps.InfoWindow({
     content: contentString
@@ -55,11 +84,17 @@ function setUpMap(lat, lng, placeId, formattedAdd)
 }
 
 function watchForm() {
-  $("form").submit((e)=>{
+  $("#js-form").submit((e)=>{
     e.preventDefault();
     const searchItem = $('#js-search-item').val();
     generateGeolocation(searchItem);
+  });
+  $("#place-form").submit((e)=>{
+    e.preventDefault();
+    const placeText = $("#place-item").val();
+    placeDetails(placeText);
   })
+
   
 }
 
